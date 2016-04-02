4de638c16e9ea4a44876bd87a4b8f46001f4be08
gabrielilharco <gabriel.ilharco@gmail.com>
2016-04-02 17:36:00 -0300

    nfa to dfa working

--- automaton.py
+++ automaton.py
@@ -8,7 +8,7 @@ class eNFA(object):
 	def __init__(self):
 		self.states = set()
 		self.alphabet = set()
-		self.transitions = {} # map de estados pra um map de alphabeto - lista de estados 
+		self.transitions = {} # map de estados pra um map de alphabeto - set de estados 
 		self.initial_state = None
 		self.terminal_states = set()
 
@@ -16,9 +16,16 @@ class NFA(object):
 	def __init__(self):
 		self.states = set()
 		self.alphabet = set()
-		self.transitions = {} # map de estados pra um map de alphabeto - lista de estados 
+		self.transitions = {} # map de estados pra um map de alphabeto - set de estados 
 		self.initial_state = None
 		self.terminal_states = set()
+
+	def add_states(self, states):
+		self.states = set(states)
+		for state in self.states:
+			self.transitions[state] = {}
+			for symbol in self.alphabet:
+				self.transitions[state][symbol] = set()
 
 class DFA(object):
 	def __init__(self):
@@ -29,8 +36,14 @@ class DFA(object):
 		self.terminal_states = set()
 
 	def print_to_file(self, filename):
-		pass
-		
+		print "Printing states:"
+		for state in self.states:
+			print state
+
+		for state in self.transitions:
+			for symbol in self.alphabet:
+				print "State: " + str(state) + " - " + symbol + " - " + str(self.transitions[state][symbol]) 
+
 class AutomatonConverter(object):
 	def dfa_from_enfa(self, enfa):
 		nfa = self.nfa_from_enfa(enfa)
@@ -42,11 +55,10 @@ class AutomatonConverter(object):
 
 	def dfa_from_nfa(self, nfa):
 		dfa = DFA()
-		dfa.initial_state = set([nfa.initial_state])
-		dfa.states = set(dfa.initial_state)
+		dfa.initial_state = frozenset([nfa.initial_state])
+		dfa.states = set([dfa.initial_state])
 		dfa.alphabet = nfa.alphabet
 		unprocessed_states = dfa.states.copy()	
-		
 		while len(unprocessed_states) > 0: 
 			current_dfa_state = unprocessed_states.pop()
 			dfa.transitions[current_dfa_state] = {}
@@ -59,10 +71,10 @@ class AutomatonConverter(object):
 				dfa.transitions[current_dfa_state][symbol] = next_states
 
 				if not next_states in dfa.states: 
-					dfa.states.add(next_states)
-					unprocessed_states.add(next_states)
+					dfa.states.add(frozenset(next_states))
+					unprocessed_states.add(frozenset(next_states))
 
 		for state in dfa.states: 
-			if state and elen(state & nfa.terminal_states) > 0: 
-				dfa.terminal_states.append(state)
+			if state and len(state & nfa.terminal_states) > 0: 
+				dfa.terminal_states.update(state)
 		return dfa
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from automaton import AutomatonConverter
+from automaton import AutomatonConverter, NFA
 
 
 def enfa_from_file(file_name):
@@ -7,8 +7,19 @@ def enfa_from_file(file_name):
 def main():
 	conv = AutomatonConverter()
 
-	enfa = enfa_from_file("enfa.txt")
-	dfa = conv.dfa_from_enfa(enfa)
+	# dummy nfa
+	nfa = NFA()
+	nfa.add_states(['q0', 'q1', 'q2'])
+	nfa.alphabet = set(['a', 'b'])
+	nfa.transitions['q0']['a'] = set(['q0','q1'])
+	nfa.transitions['q0']['b'] = set(['q2'])
+	nfa.transitions['q1']['a'] = set(['q0'])
+	nfa.transitions['q1']['b'] = set(['q1','q2'])
+	nfa.transitions['q2']['a'] = set(['q1'])
+	nfa.transitions['q2']['b'] = set(['q0','q2'])
+	nfa.initial_state = 'q0'
+	nfa.terminal_states = set(['q1'])
+	dfa = conv.dfa_from_nfa(nfa)
 	dfa.print_to_file("dfa.txt")
 
 if __name__ == '__main__':
