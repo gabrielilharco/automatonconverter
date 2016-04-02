8b6210e51597c534f4c608ce6a2ee1fcd12e08ee
gabrielilharco <gabriel.ilharco@gmail.com>
2016-04-02 16:51:05 -0300

    implementing dfa from nfa

--- automaton.py
+++ automaton.py
@@ -28,16 +28,41 @@ class DFA(object):
 		self.initial_state = None
 		self.terminal_states = set()
 
-
+	def print_to_file(self, filename):
+		pass
+		
 class AutomatonConverter(object):
 	def dfa_from_enfa(self, enfa):
-		nfa = nfa_from_enfa(enfa)
-		dfa = dfa_from_nfa(nfa)
-
+		nfa = self.nfa_from_enfa(enfa)
+		dfa = self.dfa_from_nfa(nfa)
 		return dfa
 
 	def nfa_from_enfa(self, enfa):
-		pass
+		return NFA()
 
 	def dfa_from_nfa(self, nfa):
-		pass
+		dfa = DFA()
+		dfa.initial_state = set([nfa.initial_state])
+		dfa.states = set(dfa.initial_state)
+		dfa.alphabet = nfa.alphabet
+		unprocessed_states = dfa.states.copy()	
+		
+		while len(unprocessed_states) > 0: 
+			current_dfa_state = unprocessed_states.pop()
+			dfa.transitions[current_dfa_state] = {}
+			
+			for symbol in dfa.alphabet: 
+				next_states = set()
+				for nfa_state in current_dfa_state:
+					next_states.update(nfa.transitions[nfa_state][symbol])
+				
+				dfa.transitions[current_dfa_state][symbol] = next_states
+
+				if not next_states in dfa.states: 
+					dfa.states.add(next_states)
+					unprocessed_states.add(next_states)
+
+		for state in dfa.states: 
+			if state and elen(state & nfa.terminal_states) > 0: 
+				dfa.terminal_states.append(state)
+		return dfa
