99d90ff500c76a8c00a74858e70cc76b3153fa1c
Alexandre Muzio <ax.muzio@gmail.com>
2016-04-02 18:34:00 -0300

    eNFA to NFA conversion

--- automaton.py
+++ automaton.py
@@ -4,13 +4,36 @@
 # n linhas de k colunas+1 colunas, contendo:
 # nome do estado e as transições, separados por ;
 
+import copy
+
 class eNFA(object):
-	def __init__(self):
-		self.states = set()
-		self.alphabet = set()
-		self.transitions = {} # map de estados pra um map de alphabeto - set de estados 
-		self.initial_state = None
-		self.terminal_states = set()
+	def __init__(self, states, alphabet,
+				transitions, initial_state,
+				terminal_states):
+
+		self.states = set(states)
+		self.alphabet = set(alphabet)
+		self.transitions = transitions  # map de estados pra um map de alphabeto - set de estados 
+		self.initial_state = initial_state
+		self.terminal_states = terminal_states
+
+	def e_closure(self, state):
+		e_clo = set()
+		#e_clo.add(state)
+
+		stack = []
+		stack.append(state)
+		while(len(stack) > 0):
+			curr = stack.pop()
+
+			if 'e' not in self.transitions[curr].keys(): continue
+
+			for e_tr in self.transitions[curr]['e']:
+				if (e_tr not in e_clo):
+					stack.append(e_tr)
+					e_clo.add(e_tr)
+
+		return e_clo
 
 class NFA(object):
 	def __init__(self):
@@ -35,7 +58,7 @@ class DFA(object):
 		self.initial_state = None
 		self.terminal_states = set()
 
-	def print_to_file(self, filename):
+	def output_to_file(self, filename):
 		print "Printing states:"
 		for state in self.states:
 			print state
@@ -48,10 +71,38 @@ class AutomatonConverter(object):
 	def dfa_from_enfa(self, enfa):
 		nfa = self.nfa_from_enfa(enfa)
 		dfa = self.dfa_from_nfa(nfa)
+
 		return dfa
 
 	def nfa_from_enfa(self, enfa):
-		return NFA()
+		nfa = NFA()
+		nfa.states = enfa.states
+		nfa.alphabet = enfa.alphabet
+		nfa.initial_state = enfa.initial_state
+		nfa.terminal_states = enfa.terminal_states
+
+		nfa.transitions = copy.deepcopy(enfa.transitions)
+		
+		#Remove eps transitions
+		for s, tr in nfa.transitions.iteritems():
+			del tr['e']
+
+		for s in enfa.states:
+			e_closure = enfa.e_closure(s)
+
+			#Check if s is terminal state
+			for ter_s in enfa.terminal_states:
+				if ter_s in e_closure:
+					nfa.terminal_states.append(s)
+					break
+
+			for s_e in e_closure:
+				for symbol, s_trs in enfa.transitions[s_e].iteritems():
+					if (symbol == 'e'): continue
+					for s_tr in s_trs:
+						nfa.transitions[s][symbol].append(s_tr)
+				
+		return nfa
 
 	def dfa_from_nfa(self, nfa):
 		dfa = DFA()
--- main.py
+++ main.py
@@ -1,26 +1,59 @@
-from automaton import AutomatonConverter, NFA
+from automaton import *
 
+def enfa_from_file(fname):
+	content = [line.rstrip('\n') for line in open(fname)]
 
-def enfa_from_file(file_name):
-	pass
+	states_list = [s for s in content[0].split('\t')]
+	alph_list = [s for s in content[1].split('\t')]
+	initial_state = content[2].split('\t')
+	terminal_states = [s for s in content[3].split(';')]
+
+	transitions = {}
+	for i in range(len(states_list)):
+		t_line = content[i + 4].split('\t')
+		ori = states_list[i]
+
+		transitions[ori] = {}
+
+		for idx, t_col in enumerate(t_line):
+			trs = t_col.split(';')
+			
+			transitions[ori][alph_list[idx]] = []
+			for j in range(len(trs)):
+				if (trs[j] == '-'): continue
+				transitions[ori][alph_list[idx]].append(trs[j])
+		# print states_list
+		# print alph_list
+		# print initial_state
+		# print terminal_states
+		# print transitions
+
+	return eNFA(states_list, alph_list,
+			transitions, initial_state,
+			terminal_states)
+
 
 def main():
 	conv = AutomatonConverter()
+	enfa = enfa_from_file("test/entr1.txt")
 
-	# dummy nfa
-	nfa = NFA()
-	nfa.add_states(['q0', 'q1', 'q2'])
-	nfa.alphabet = set(['a', 'b'])
-	nfa.transitions['q0']['a'] = set(['q0','q1'])
-	nfa.transitions['q0']['b'] = set(['q2'])
-	nfa.transitions['q1']['a'] = set(['q0'])
-	nfa.transitions['q1']['b'] = set(['q1','q2'])
-	nfa.transitions['q2']['a'] = set(['q1'])
-	nfa.transitions['q2']['b'] = set(['q0','q2'])
-	nfa.initial_state = 'q0'
-	nfa.terminal_states = set(['q1'])
-	dfa = conv.dfa_from_nfa(nfa)
-	dfa.print_to_file("dfa.txt")
+	# # dummy nfa
+	# nfa = NFA()
+	# nfa.add_states(['q0', 'q1', 'q2'])
+	# nfa.alphabet = set(['a', 'b'])
+	# nfa.transitions['q0']['a'] = set(['q0','q1'])
+	# nfa.transitions['q0']['b'] = set(['q2'])
+	# nfa.transitions['q1']['a'] = set(['q0'])
+	# nfa.transitions['q1']['b'] = set(['q1','q2'])
+	# nfa.transitions['q2']['a'] = set(['q1'])
+	# nfa.transitions['q2']['b'] = set(['q0','q2'])
+	# nfa.initial_state = 'q0'
+	# nfa.terminal_states = set(['q1'])
+	# dfa = conv.dfa_from_nfa(nfa)
+	# dfa.output_to_file("dfa.txt")
+
+	# dfa = conv.dfa_from_enfa(enfa)
+	#dfa.output_to_file("dfa.txt")
 
 if __name__ == '__main__':
 	main()
